37a5c3fa494ed06e91ffd6ff0687b71fa2d16d76
DmitrySmirnOFFF <99009770+DmitrySmirnOFFF@users.noreply.github.com>
2024-02-21 15:31:19 +0300

    кубик макет (-докчпок)

--- докчпок/LCD_128x64/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "D:\ATMEL\LCD_128x64\labels.tmp" -fI -W+ie -o "D:\ATMEL\LCD_128x64\LCD_128x64.hex" -d "D:\ATMEL\LCD_128x64\LCD_128x64.obj" -e "D:\ATMEL\LCD_128x64\LCD_128x64.eep" -m "D:\ATMEL\LCD_128x64\LCD_128x64.map" "D:\ATMEL\LCD_128x64\LCD_128x64.asm"
